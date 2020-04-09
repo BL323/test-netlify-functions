addded959a9ef3675391e241cc160aad81d3b869
Ben Lourence <ben@lourence.com>
2020-04-09 12:56:53 +0100

    Adopted zipped folder structure.

--- lambda/weather/weather.js
+++ lambda/weather/weather.js
@@ -5,17 +5,18 @@ const { API_KEY } = process.env;
 exports.handler = async function(event, context) {
     const city = event.queryStringParameters.city || 'London';
     const requestUrl = `${API_ENDPOINT}${city},uk&APPID=${API_KEY}`;
+    
     const response = await fetch(requestUrl, {
-      headers: { Accept: 'application/json' }
-    })
-    if (!response.ok) {
-      return { statusCode: response.status, body: response.statusText }
-    }
-
-    const data = await response.json()
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ msg: data })
-    }
+        headers: { Accept: 'application/json' }
+      })
+      if (!response.ok) {
+        return { statusCode: response.status, body: response.statusText }
+      }
+      
+      const data = await response.json()
+      
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ msg: data })
+      }    
 }
